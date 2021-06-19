f07bc4f19eba37e34f3dea2cab3ad55d5fd0c974
Katsuyuki Oeda <katsu.amagramer@gmail.com>
2021-06-19 22:10:27 +0900

    change manifest folder to watch and copy them.

--- background/vite.config.ts
+++ background/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import path from "path";
+import copy from "rollup-plugin-copy";
+
+const outDir = path.resolve(__dirname, "../dist");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: path.resolve(__dirname, "../dist"),
+    outDir,
     lib: {
       entry: path.resolve(__dirname, "src/background.ts"),
       name: "background",
@@ -19,4 +22,14 @@ export default defineConfig({
       },
     },
   },
+  plugins: [
+    copy({
+      targets: [
+        {
+          src: path.join(__dirname, "manifests/*").split(path.sep).join("/"),
+          dest: outDir,
+        },
+      ],
+    }),
+  ],
 });
